33c73ef516634fb19b996c3c1f89d6b6214efa33
Michael Fisher <mirfishe@gmail.com>
2020-09-08 15:53:25 -0400

    added tentative geolocation code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import NASA from './components/NASA/NASA';
@@ -6,11 +6,54 @@ import OpenWeather from  './components/OpenWeather/OpenWeather';
 import Zomato from './components/Zomato/Zomato';
 
 function App() {
+
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  };
+
+  const showPosition = (position) => {
+    // console.log("Latitude: ", position.coords.latitude, "Longitude: " + position.coords.longitude);
+    setLatitude(position.coords.latitude);
+    setLongitude(position.coords.longitude);
+  };
+
+  useEffect(() => {
+    getLocation();
+    // console.log("Latitude: ", latitude, "Longitude: " + longitude);
+}, []);
+
+useEffect(() => {
+  console.log("Latitude: ", latitude, "Longitude: " + longitude);
+}, [latitude, longitude]);
+
+    //   const showPosition = (position) => {
+    //     setLatitude(position.coords.latitude);
+    //     setLongitude(position.coords.longitude);
+    //   };
+
+    //   useEffect(() => {
+
+    //     if (navigator.geolocation) {
+    //       navigator.geolocation.getCurrentPosition(showPosition);
+    //     } else {
+    //       console.log("Geolocation is not supported by this browser.");
+    //     };
+
+    //     console.log("Latitude: ", latitude, "Longitude: " + longitude);
+    // }, []);
+
   return (
     <div className="App">
-      <NASA />
-      <OpenWeather />
-      <Zomato />
+      <NASA latitude={latitude} longitude={longitude} />
+      <OpenWeather latitude={latitude} longitude={longitude} />
+      <Zomato latitude={latitude} longitude={longitude} />
     </div>
   );
 }
--- src/components/Zomato/Zomato.js
+++ src/components/Zomato/Zomato.js
@@ -1,8 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {Container} from 'reactstrap';
 import './Zomato.css';
 
 const Zomato = (props) => {
+
+    const [latitude, setLatitude] = useState(props.latitude);
+    const [longitude, setLongitude] = useState(props.longitude);
+    const [restaurants, setRestaurants] = useState([]);
+
+    // const getLocation = () => {
+    //     if (navigator.geolocation) {
+    //       navigator.geolocation.getCurrentPosition(showPosition);
+    //     } else {
+    //       console.log("Geolocation is not supported by this browser.");
+    //     }
+    //   };
+
+    //   function showPosition(position) {
+    //     console.log("Latitude: ", position.coords.latitude, "Longitude: " + position.coords.longitude);
+    //   };
+
+    const fetchRestaurants = () => {
+
+        fetch(`https://developers.zomato.com/api/v2.1/search?lat=${latitude}&lon=${longitude}`, {
+            method: 'GET',
+            headers:    new Headers ({
+                'Content-Type': 'application/json',
+                'user-key': process.env.REACT_APP_ZOMATO_API_KEY
+            })
+        })
+        .then(res => res.json())
+        .then(restaurantData => console.log(restaurantData)) //{setWorkouts(logData); console.log(logData); console.log(logData.message); console.log(logData.logs); }
+        .catch(err => console.log(err))
+        
+    };
+
+    useEffect(() => {
+        // getLocation();
+        fetchRestaurants();
+    }, [props.latitude, props.longitude]);
+
 
     return (
         <Container>
