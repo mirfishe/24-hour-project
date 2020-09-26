bc31ce5ce58c65f2f7711b4fd4536238eb86c726
Michael Fisher <mirfishe@gmail.com>
2020-09-26 14:37:03 -0400

    Updates

---
+++ precache-manifest.8ed0bef5a76bcaaf25a55abfe94a9b10.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b5622dd8cff40459d79b6b8004a1490",
+    "url": "/24-hour-project/index.html"
+  },
+  {
+    "revision": "6916b3b9afe408a4cf08",
+    "url": "/24-hour-project/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "c7ab02cfa40564e7b27d",
+    "url": "/24-hour-project/static/css/main.99595a2c.chunk.css"
+  },
+  {
+    "revision": "6916b3b9afe408a4cf08",
+    "url": "/24-hour-project/static/js/2.aa6baedd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/24-hour-project/static/js/2.aa6baedd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7ab02cfa40564e7b27d",
+    "url": "/24-hour-project/static/js/main.ab868963.chunk.js"
+  },
+  {
+    "revision": "dece08ce089f8c7b6a8b",
+    "url": "/24-hour-project/static/js/runtime-main.80712e20.js"
+  },
+  {
+    "revision": "708d9ec5a72aa42bbe1961ce314ac49d",
+    "url": "/24-hour-project/static/media/Map-2-icon.708d9ec5.png"
+  }
+]);
